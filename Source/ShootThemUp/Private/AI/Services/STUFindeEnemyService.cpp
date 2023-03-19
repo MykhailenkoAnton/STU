9c67d593321ce9f5396656e3422d0f7825c3c15d
Anton Mykhailenko <mykhailencko2594@gmail.com>
2023-03-19 02:15:58 +0200

    Decarator

---
+++ Source/ShootThemUp/Private/AI/Services/STUFindeEnemyService.cpp
@@ -0,0 +1,29 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+
+#include "AI/Services/STUFindeEnemyService.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "STUUtils.h"
+#include "Components/STUAIPerceptionComponent.h"
+
+USTUFindeEnemyService::USTUFindeEnemyService() 
+{
+    NodeName = "Find Enemy";
+}
+
+void USTUFindeEnemyService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) 
+{
+    const auto BlackBoard = OwnerComp.GetBlackboardComponent();
+    if (BlackBoard)
+    {
+        const auto Controller = OwnerComp.GetAIOwner();
+        const auto PerceptionComponent = STUUtils::GetSTUPlayerComponent<USTUAIPerceptionComponent>(Controller);
+        if (PerceptionComponent)
+        {
+            BlackBoard->SetValueAsObject(EnemyActorKey.SelectedKeyName, PerceptionComponent->GetClosestEnemy());
+        }
+    }
+
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
