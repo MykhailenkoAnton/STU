6e0adf2d94135300db2d50a98339b42cb08cea1a
Anton Mykhailenko <mykhailencko2594@gmail.com>
2023-04-22 23:56:17 +0300

    modify GameModeBase

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -4,10 +4,71 @@
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
 #include "UI/STUGameHUD.h"
+#include "AIController.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogSTUGameModeBase, All, All);
 
 ASTUGameModeBase::ASTUGameModeBase()
 {
     DefaultPawnClass = ASTUBaseCharacter::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
     HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay() 
+{
+    Super::StartPlay();
+
+    SpawnBots();
+    CurrentRound = 1;
+    StartRound();
+}
+
+void ASTUGameModeBase::SpawnBots()
+{
+    if (!GetWorld()) return;
+
+    for (int32 i = 0; i < GameData.PlayersNum - 1; i++)
+    {
+        FActorSpawnParameters SpawnInfo;
+        SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+        const auto STUAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+        RestartPlayer(STUAIController);
+    }
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController) 
+{
+    if (InController && InController->IsA<AAIController>())
+    {
+        return AIPawnClass;
+    }
+
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTUGameModeBase::StartRound() 
+{
+    RounsCountDown = GameData.RoundsTime;
+    GetWorldTimerManager().SetTimer(GameRoundTimerHandle, this, &ASTUGameModeBase::GameTimerUpdate, 1.0f, true);
+}
+void ASTUGameModeBase::GameTimerUpdate() 
+{
+    UE_LOG(LogSTUGameModeBase, Display, TEXT("Time: %i / Round: %i/%i"), RounsCountDown, CurrentRound, GameData.RoundsNum);
+
+    if (--RounsCountDown == 0)
+    {
+        GetWorldTimerManager().ClearTimer(GameRoundTimerHandle);
+
+        if (CurrentRound + 1 <= GameData.RoundsNum)
+        {
+            ++CurrentRound;
+            StartRound();
+        }
+        else
+        {
+            UE_LOG(LogSTUGameModeBase, Display, TEXT("======= GAME OVER ========"));
+        }
+    }
 }
