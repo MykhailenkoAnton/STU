5c5873825f27286f71cdfc6606056ead1c0f84fe
Anton Mykhailenko <mykhailencko2594@gmail.com>
2023-04-30 17:54:18 +0300

    game over widget and player stats widget

---
+++ Source/ShootThemUp/Private/UI/STUGameOverWidget.cpp
@@ -0,0 +1,58 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+
+#include "UI/STUGameOverWidget.h"
+#include "STUGameModeBase.h"
+#include "Player/STUPlayerState.h"
+#include "UI/STUPlayerStatRowWidget.h"
+#include "Components/VerticalBox.h"
+#include "STUUtils.h"
+
+bool USTUGameOverWidget::Initialize() 
+{
+    if (GetWorld())
+    {
+        const auto GameMode = Cast<ASTUGameModeBase>(GetWorld()->GetAuthGameMode());
+        if (GameMode)
+        {
+            GameMode->OnMatchStateChange.AddUObject(this, &USTUGameOverWidget::OnMatchStateChanged);
+        }
+    }
+
+    return Super::Initialize();
+}
+
+void USTUGameOverWidget::OnMatchStateChanged(ESTUMatchState State) 
+{
+    if (State == ESTUMatchState::GameOver)
+    {
+        UpdatePlayerStat();
+    }
+}
+
+void USTUGameOverWidget::UpdatePlayerStat()
+{
+    if (!GetWorld() || !PlayerStatBox) return;
+
+    PlayerStatBox->ClearChildren();
+
+    for (auto It = GetWorld()->GetControllerIterator(); It; ++It)
+    {
+        const auto Controller = It->Get();
+        if (!Controller) continue;
+
+        const auto PlayerState = Cast<ASTUPlayerState>(Controller->PlayerState);
+        if (!PlayerState) continue;
+
+        const auto PlayerStatRowWidget = CreateWidget<USTUPlayerStatRowWidget>(GetWorld(), PlayerStatsRowWidgetClass);
+        if (!PlayerStatRowWidget) continue;
+
+        PlayerStatRowWidget->SetPlayerName(FText::FromString(PlayerState->GetPlayerName()));
+        PlayerStatRowWidget->SetKills(STUUtils::TextFromInt(PlayerState->GetKillsNum()));
+        PlayerStatRowWidget->SetDeath(STUUtils::TextFromInt(PlayerState->GetDeathNum()));
+        PlayerStatRowWidget->SetTeam(STUUtils::TextFromInt(PlayerState->GetTeamID()));
+        PlayerStatRowWidget->SetPlayerIndicatorVisibility(Controller->IsPlayerController());
+
+        PlayerStatBox->AddChild(PlayerStatRowWidget);
+    }
+}
---
+++ Source/ShootThemUp/Public/UI/STUGameOverWidget.h
@@ -0,0 +1,33 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STUCoreTypes.h"
+#include "STUGameOverWidget.generated.h"
+
+class UVerticalBox;
+
+UCLASS()
+class SHOOTTHEMUP_API USTUGameOverWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+    virtual bool Initialize() override;
+
+protected:
+
+    UPROPERTY(meta = (BindWidget))
+    UVerticalBox* PlayerStatBox;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+    TSubclassOf<UUserWidget> PlayerStatsRowWidgetClass;
+
+private:
+
+    void OnMatchStateChanged(ESTUMatchState State);
+
+    void UpdatePlayerStat();
+};
