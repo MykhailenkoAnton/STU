e252a5fce53c5afcc9765a7532ce38e050699441
MykhailenkoAnton <mykhailencko2594@gmail.com>
2023-05-11 00:14:30 +0300

    FuncSoundLib

--- Source/ShootThemUp/Private/STUGameInstance.cpp
+++ Source/ShootThemUp/Private/STUGameInstance.cpp
@@ -1,3 +1,9 @@
 // Shoot Them Up Game, All Rights Reserved.
 
 #include "STUGameInstance.h"
+#include "Sound/STUSoundFuncLib.h"
+
+void USTUGameInstance::ToogleVolume() 
+{
+    USTUSoundFuncLib::ToggleSoundClassVolume(MasterSoundClass);
+}
